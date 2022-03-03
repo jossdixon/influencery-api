bf655d518837e410e20f3bdf0c51d5536aee478b
Joss Dixon <josscdixon@gmail.com>
2022-03-03 13:57:31 +0000

    fixed primary tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,4 @@
 class Tag < ApplicationRecord
     has_many :influencer_tags
     has_many :influencers, through: :influencer_tags
-
-    has_many :primary_tags, class_name: "Tag", foreign_key: "primary_tag_id"
 end

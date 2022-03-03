1074c5cbfcc209bae180935bc6e0463b561bbb05
Joss Dixon <josscdixon@gmail.com>
2022-03-03 13:52:58 +0000

    added primary tags to tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
     has_many :influencer_tags
-
     has_many :influencers, through: :influencer_tags
+
+    has_many :primary_tags, class_name: "Tag", foreign_key: "primary_tag_id"
 end

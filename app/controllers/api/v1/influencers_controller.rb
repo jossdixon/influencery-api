81b0c32af595899915e1278cff67982dec897c51
Joss Dixon <josscdixon@gmail.com>
2022-03-03 11:55:46 +0000

    seeded

--- app/controllers/api/v1/influencers_controller.rb
+++ app/controllers/api/v1/influencers_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::InfluencersController < ApplicationController
 
+  def index
+    @influencers = Influencer.all
+    render json: @influencers
+  end
+
+  def show
+    @influencer = Influencer.find(params[:id])
+  end
 end
---
+++ db/migrate/20220303105138_add_primary_tag_to_influencers.rb
@@ -0,0 +1,5 @@
+class AddPrimaryTagToInfluencers < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :influencers, :primary_tag, foreign_key: { to_table: :tags }
+  end
+end
